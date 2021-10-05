72ec59dc7538f7af11c09d7e1085aa42b1eae6c8
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-10-05 09:44:49 -0700

    set required services

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require("mongoose");
+app.use(cookieParser);
+app.use(express.json());
+ 
+const server = app.listen(process.env.PORT || 8080, () =>
+ console.log(`Listening port ${process.env.PORT || 8080}`)
+);
