9baeaff95ba3470c71e310cc114ec7e2b0b04bda
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-10-05 11:39:35 -0700

    deleting testing files

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 app.use(cookieParser);
 app.use(express.json());
 
-
+mongoose.connect(
+  "mongodb+srv://javiergongora:DBatlass7769@cluster0.6v3kl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => {
+    console.log("Successfully connected to Atlas database");
+  }
+);
 
 const server = app.listen(process.env.PORT || 8080, () =>
- console.log(`Listening port ${process.env.PORT || 8080}`)
+  console.log(`Express server started, listening port ${process.env.PORT || 8080}`)
 );
