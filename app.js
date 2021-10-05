048c4d5c018080d4b5d3320a93ee678d7ec7a379
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-10-05 11:24:28 -0700

    delete db connection

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const cookieParser = require('cookie-parser');
 const mongoose = require("mongoose");
 app.use(cookieParser);
 app.use(express.json());
- 
+
+
+
 const server = app.listen(process.env.PORT || 8080, () =>
  console.log(`Listening port ${process.env.PORT || 8080}`)
 );
