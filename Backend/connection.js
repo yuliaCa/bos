97ea904b9b044f70a2bc6f76d0cc717d93445d0f
Mark Joseph Suria <msuria00@mylangara.ca>
2021-10-20 16:38:10 -0700

    Fix: Rename folder from db to Backend

--- Backend/connection.js
+++ Backend/connection.js
@@ -2,13 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const mongoose = require("mongoose");
-//require("dotenv").config();
+require("dotenv").config();
 
 let mongoDB = `mongodb+srv://bosadmin:WtfqXj6G0MY@cluster0.6v3kl.mongodb.net/bosDB?retryWrites=true&w=majority`;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    //console.log("Connected to Atlas!");
-    // const server = app.listen(8080, () => console.log("Listening"));
-});
 
-module.exports = mongoose.connection;
+
+module.exports = mongoose.connect(mongoDB);
