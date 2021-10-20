3f19f0389bef37568a8688b252dd6e054556ba56
Doug Leung <dleung46@mylangara.ca>
2021-10-19 18:20:59 -0700

    feat: to add InsightsSection to HomePage; issue10

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroSection from "../components/HeroSection/HeroSection";
 import FeaturesSection from "../components/FeaturesSection/FeaturesSection";
+import InsightsSection from "../components/InsightsSection/InsightsSection";
 
 function HomePage() {
     return <div>
@@ -8,6 +9,8 @@ function HomePage() {
         <HeroSection src="https://s3-us-west-2.amazonaws.com/bos-skincare/testing-image.jpg" alt="hero image" heading="We talk about website here" bodytext="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae laboriosam obcaecati exercitationem harum rerum veniam consectetur excepturi, neque hic, aliquid ipsum voluptate modi nostrum odit quidem provident autem dolorum. Fugit, non aliquid necessitatibus molestiae accusamus velit. Corporis dignissimos voluptates quaerat, assumenda incidunt quis exercitationem iure distinctio vitae accusamus asperiores consequatur."/>
 
         <FeaturesSection />
+
+        <InsightsSection src="https://picsum.photos/300" alt="insights image" heading="We talk about insights here" bodytext="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae laboriosam obcaecati exercitationem harum rerum veniam consectetur excepturi, neque hic, aliquid ipsum voluptate modi nostrum odit quidem provident autem dolorum. Fugit, non aliquid necessitatibus molestiae accusamus velit. Corporis dignissimos voluptates quaerat, assumenda incidunt quis exercitationem iure distinctio vitae accusamus asperiores consequatur."/>
        
         </div>
 }
