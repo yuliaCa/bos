94d7aeec1c9f04633ad5befa964422266e9bdb37
Mark Joseph Suria <msuria00@mylangara.ca>
2021-10-20 16:56:42 -0700

    fix: Made changes in the connection function

--- Backend/connection.js
+++ Backend/connection.js
@@ -6,6 +6,9 @@ require("dotenv").config();
 
 let mongoDB = `mongodb+srv://bosadmin:WtfqXj6G0MY@cluster0.6v3kl.mongodb.net/bosDB?retryWrites=true&w=majority`;
 
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    //console.log("Connected to Atlas!");
+    // const server = app.listen(8080, () => console.log("Listening"));
+});
 
-
-module.exports = mongoose.connect(mongoDB);
+module.exports = mongoose.connection;
--- app.js
+++ app.js
@@ -5,7 +5,8 @@ app.use(cookieParser);
 app.use(express.json());
 const connected = require("./Backend/connection.js");
 
-connected.then(() => {
+
+connected.on('open', () => {
     console.log("Connected to Atlas!");
     const server = app.listen(8080, () => console.log("Listening"));
 });
