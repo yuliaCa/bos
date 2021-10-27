bb0c6203e558fcd97252da959137028aacd69577
Mark Joseph Suria <msuria00@mylangara.ca>
2021-10-26 17:23:52 -0700

    Update: update userProfileSchema for registration

--- Backend/models/userProfileSchema.js
+++ Backend/models/userProfileSchema.js
@@ -11,20 +11,32 @@ const productSubSchema = new dbSchema({
 });
 
 
+const skintypeSubSchema = new dbSchema({
+    dry: { type: Boolean, default: false },
+    normal: { type: Boolean, default: false },
+    combination: { type: Boolean, default: false },
+    sensitive: { type: Boolean, default: false }
+});
+
+const concernsSubSchema = new dbSchema({
+    acne: { type: Boolean, default: false },
+    dryness: { type: Boolean, default: false },
+    oilyness: { type: Boolean, default: false },
+    blemishes: { type: Boolean, default: false },
+    //"dark spots": input["dark spots"],
+    pores: { type: Boolean, default: false },
+    //"red lines": input["red lines"],
+    //"fine lines": input["fine lines"]
+});
+
+
 let userProfileSchema = new dbSchema({
     emailAddress: { type: String, lowercase: true, required: true },
     fullname: { type: String, required: true },
     gender: { type: String },
     cityLocation: { type: String },
-    dry: { type: Boolean, default: false },
-    normal: { type: Boolean, default: false },
-    combination: { type: Boolean, default: false },
-    sensitive: { type: Boolean, default: false },
-    acne: { type: Boolean, default: false },
-    dryness: { type: Boolean, default: false },
-    oilyness: { type: Boolean, default: false },
-    blemishes: { type: Boolean, default: false },
-    pores: { type: Boolean, default: false },
+    skintype: [skintypeSubSchema],
+    concerns: [concernsSubSchema],
     birthDate: { type: Date, default: Date.now },
     objMorningRoutineLog: [productSubSchema],
     objEveningRoutineLog: [productSubSchema],
