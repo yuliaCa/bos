0d7f4080dacf671029a43e3e635e29324949b747
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-10-28 11:00:58 -0700

    charts: hide background grid and maintain aspect ratio.

--- client/src/components/Charts/UsageChart.js
+++ client/src/components/Charts/UsageChart.js
@@ -12,22 +12,36 @@ const UsageChart = () => {
             {
               label: "Products Usage",
               data: [4, 2, 3, 3, 4, 4, 2],
-              backgroundColor: "rgba(255, 206, 86, 0.3)",
+              backgroundColor: "rgba(209, 152, 136, 0.3)",
               borderColor:"rgba(255, 206, 86, 1)",
-              borderWidth: 1,
+              borderWidth: 0,
             },
           ],
         }}
-        height={400}
+        height={300}
         width={600}
         options={{
-          maintainAspectRatio: false,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              top: 50,
+              left: 200,
+              right: 230,
+              bottom: 50,
+            },
+          },
           scales: {
+            xAxes: [
+              {
+                gridLines: false,
+              },
+            ],
             yAxes: [
               {
                 ticks: {
                   beginAtZero: true,
                 },
+                gridLines: false,
               },
             ],
           },
--- client/src/components/Charts/WeatherChart.js
+++ client/src/components/Charts/WeatherChart.js
@@ -2,7 +2,28 @@ import styles from "./WeatherChart.module.css";
 import React from "react";
 import { Bar } from "react-chartjs-3";
 
-const rand = () => Math.round(Math.random() * 10);
+const randT = () => Math.round(Math.random() * 31);
+const randH = () => Math.round(Math.random() * 100);
+
+let temperatureData = [
+  randT(),
+  randT(),
+  randT(),
+  randT(),
+  randT(),
+  randT(),
+  randT(),
+];
+
+let humidityData = [
+  randH(),
+  randH(),
+  randH(),
+  randH(),
+  randH(),
+  randH(),
+  randH(),
+];
 
 const data = {
   labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
@@ -13,7 +34,7 @@ const data = {
       backgroundColor: "rgba(243, 186, 124, 0.5)",
       borderColor: "rgba(243, 186, 124, 0)",
       borderWidth: 0,
-      data: [rand(), rand(), rand(), rand(), rand(), rand(), rand()],
+      data: temperatureData,
     },
     {
       type: "line",
@@ -21,7 +42,7 @@ const data = {
       borderColor: "rgb(209, 152, 136)",
       borderWidth: 2,
       fill: false,
-      data: [rand(), rand(), rand(), rand(), rand(), rand(), rand()],
+      data: humidityData,
     },
   ],
 };
@@ -31,31 +52,42 @@ const WeatherChart = () => {
     <div className={styles.chartStyle}>
       <Bar
         data={data}
-        height={400}
+        height={300}
         width={600}
         options={{
           maintainAspectRatio: true,
           layout: {
             padding: {
-              top: 100,
+              top: 50,
               left: 200,
               right: 200,
-              bottom: 100,
+              bottom: 50,
             },
           },
           scales: {
+            xAxes: [
+              {
+                gridLines: false,
+              },
+            ],
             yAxes: [
+              {
+                position: "right",
+                ticks: {
+                  beginAtZero: true,
+                  steps: 3,
+                  stepsValue: 33,
+                  max: 99,
+                },
+                gridLines: false,
+                suggestedMax: 35,
+              },
               {
                 position: "left",
                 ticks: {
                   beginAtZero: true,
                 },
-              },
-              {
-                position: "right",
-                ticks: {
-                  beginAtZero: true,
-                },
+                gridLines: false,
               },
             ],
           },
