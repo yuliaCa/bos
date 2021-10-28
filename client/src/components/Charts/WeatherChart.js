ea1148f3252259a33b4956f0d433f6a83cd6b987
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-10-28 09:36:49 -0700

    combine bar chart and line chart with two datasets

--- client/src/components/Charts/WeatherChart.js
+++ client/src/components/Charts/WeatherChart.js
@@ -2,29 +2,57 @@ import styles from "./WeatherChart.module.css";
 import React from "react";
 import { Bar } from "react-chartjs-3";
 
+const rand = () => Math.round(Math.random() * 10);
+
+const data = {
+  labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  datasets: [
+    {
+      type: "bar",
+      label: "Temperature",
+      backgroundColor: "rgba(243, 186, 124, 0.5)",
+      borderColor: "rgba(243, 186, 124, 0)",
+      borderWidth: 0,
+      data: [rand(), rand(), rand(), rand(), rand(), rand(), rand()],
+    },
+    {
+      type: "line",
+      label: "Humidity",
+      borderColor: "rgb(209, 152, 136)",
+      borderWidth: 2,
+      fill: false,
+      data: [rand(), rand(), rand(), rand(), rand(), rand(), rand()],
+    },
+  ],
+};
+
 const WeatherChart = () => {
   return (
     <div className={styles.chartStyle}>
       <Bar
-        data={{
-          labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-          datasets: [
-            {
-              label: "Weather",
-              data: [24, 62, 39, 43, 23, 76, 34],
-              backgroundColor: "rgba(54, 162, 235, 0.2)",
-              borderColor: "rgba(54, 162, 235, 1)",
-              borderWidth: 1,
-            },
-          ],
-        }}
+        data={data}
         height={400}
         width={600}
         options={{
-          maintainAspectRatio: false,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              top: 100,
+              left: 200,
+              right: 200,
+              bottom: 100,
+            },
+          },
           scales: {
             yAxes: [
               {
+                position: "left",
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+              {
+                position: "right",
                 ticks: {
                   beginAtZero: true,
                 },
