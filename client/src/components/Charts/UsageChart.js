4cfe9bb5bfc6882d4bbc251d80ac6fdeb5f3b351
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-10-28 21:49:28 -0700

    Product Chart: add stacked bars and product categories

--- client/src/components/Charts/UsageChart.js
+++ client/src/components/Charts/UsageChart.js
@@ -10,10 +10,31 @@ const UsageChart = () => {
           labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
           datasets: [
             {
-              label: "Products Usage",
-              data: [4, 2, 3, 3, 4, 4, 2],
-              backgroundColor: "rgba(209, 152, 136, 0.3)",
-              borderColor:"rgba(255, 206, 86, 1)",
+              label: "Cleanser",
+              data: [1, 2, 1, 2, 1, 0, 1],
+              backgroundColor: "rgba(243, 186, 124, 0.5)",
+              borderColor: "rgba(243, 186, 124, 0.5)",
+              borderWidth: 0,
+            },
+            {
+              label: "Serum",
+              data: [1, 1, 1, 0, 1, 0, 1],
+              backgroundColor: "rgba(209, 152, 136, 0.5)",
+              borderColor: "rgba(209, 152, 136, 0.5)",
+              borderWidth: 0,
+            },
+            {
+              label: "Toner",
+              data: [1, 0, 1, 1, 1, 0, 1],
+              backgroundColor: "rgba(182, 139, 125, 0.7)",
+              borderColor: "rgba(182, 139, 125, 0.7)",
+              borderWidth: 0,
+            },
+            {
+              label: "Cream",
+              data: [1, 1, 1, 0, 1, 1, 0],
+              backgroundColor: "rgba(247, 237, 225, 1)",
+              borderColor: "rgba(247, 237, 225, 1)",
               borderWidth: 0,
             },
           ],
@@ -26,22 +47,32 @@ const UsageChart = () => {
             padding: {
               top: 50,
               left: 200,
-              right: 230,
+              right: 200,
               bottom: 50,
             },
           },
           scales: {
             xAxes: [
               {
-                gridLines: false,
+                stacked: true,
+                gridLines: {
+                  display: true,
+                  drawBorder: true,
+                  drawOnChartArea: false,
+                },
               },
             ],
             yAxes: [
               {
+                stacked: true,
                 ticks: {
                   beginAtZero: true,
                 },
-                gridLines: false,
+                gridLines: {
+                  display: true,
+                  drawBorder: true,
+                  drawOnChartArea: false,
+                },
               },
             ],
           },
