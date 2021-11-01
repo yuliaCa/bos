44b62f0ed154e33249d8d7ffb2286a607de80cef
Yulia <mail4yulia@yahoo.com>
2021-10-31 22:30:10 -0700

    added evening routine section in My Routines

--- client/src/pages/profilePage/MyRoutines.js
+++ client/src/pages/profilePage/MyRoutines.js
@@ -2,15 +2,13 @@ import styles from './MyRoutines.module.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Select from 'react-select';
-import ProductCard from './MyRoutine/ProductCard';
-import SearchInput from './MyRoutine/ProductAutocomplete/SearchInput';
+import ProductCard from '../../components/MyRoutine/ProductCard';
+import SearchInput from '../../components/MyRoutine/ProductAutocomplete/SearchInput';
 
 
 function MyRoutines() {
 
-
     const [loadedProducts, setLoadedProducts] = useState([]);
-
 
     useEffect(() => {
 
@@ -22,7 +20,6 @@ function MyRoutines() {
             .catch(error => console.log(error));
 
     }, [])
-
 
     const [checkedAll, setCheckedAll] = useState(false);
 
@@ -37,43 +34,74 @@ function MyRoutines() {
         { label: "Mask", value: "mask" },
         { label: "Eyecare", value: "eyecare" },
         { label: "Sunscreen", value: "sunscreen" }
-
     ]
-
-
 
     return (
         <div>
-            <h1 className={styles.heading}>Morning Routine  </h1>
+            <div className={styles.morningRoutine}>
+                <h1 className={styles.heading}>Morning Routine  </h1>
 
-            <div className={styles.userInput}>
-                <Select options={categoryOptions} />
+                <div className={styles.userInput}>
+                    <Select options={categoryOptions} />
 
-                <SearchInput />
+                    <SearchInput />
 
-                <button>Add Product</button>
+                    <button>Add Product</button>
+                </div>
+
+                <div className={styles.selectAll}>
+                    <label className={styles.selectAllLabel}> Select All
+                    <input className={styles.selectAllInput} type="checkbox" value={checkedAll} onChange={checkAllHandler} />
+                    </label>
+                </div>
+
+                <div className={styles.productsGrid}>
+                    {loadedProducts.map((eachProduct) => (
+                        <ProductCard
+                            key={eachProduct._id}
+                            id={eachProduct._id}
+                            image={eachProduct.images}
+                            category={eachProduct.productCategory}
+                            name={eachProduct.productName}
+                            description={eachProduct.productDescription}
+                            checkAll={checkedAll}
+                        />
+                    ))}
+                </div>
+                <button className={styles.saveButton}>Save</button>
             </div>
+            <div className={styles.eveningRoutine}>
+                <h1 className={styles.heading}>Evening Routine  </h1>
 
-            <div className={styles.selectAll}>
-                <label className={styles.selectAllLabel}> Select All
-                <input className={styles.selectAllInput} type="checkbox" value={checkedAll} onChange={checkAllHandler} />
-                </label>
-            </div>
+                <div className={styles.userInput}>
+                    <Select options={categoryOptions} />
 
-            <div className={styles.productsGrid}>
-                {loadedProducts.map((eachProduct) => (
-                    <ProductCard
-                        key={eachProduct._id}
-                        id={eachProduct._id}
-                        image={eachProduct.images}
-                        category={eachProduct.productCategory}
-                        name={eachProduct.productName}
-                        description={eachProduct.productDescription}
-                        checkAll={checkedAll}
-                    />
-                ))}
+                    <SearchInput />
+
+                    <button>Add Product</button>
+                </div>
+
+                <div className={styles.selectAll}>
+                    <label className={styles.selectAllLabel}> Select All
+                    <input className={styles.selectAllInput} type="checkbox" value={checkedAll} onChange={checkAllHandler} />
+                    </label>
+                </div>
+
+                <div className={styles.productsGrid}>
+                    {loadedProducts.map((eachProduct) => (
+                        <ProductCard
+                            key={eachProduct._id}
+                            id={eachProduct._id}
+                            image={eachProduct.images}
+                            category={eachProduct.productCategory}
+                            name={eachProduct.productName}
+                            description={eachProduct.productDescription}
+                            checkAll={checkedAll}
+                        />
+                    ))}
+                </div>
+                <button className={styles.saveButton}>Save</button>
             </div>
-            <button className={styles.saveButton} >Save</button>
         </div>
     )
 }
