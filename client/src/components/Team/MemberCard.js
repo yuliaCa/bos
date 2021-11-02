98819492bd635596c9baad9b71537ba234915778
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-11-02 09:54:47 -0700

    Update MemberCard.js

--- client/src/components/Team/MemberCard.js
+++ client/src/components/Team/MemberCard.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./MemberCard.module.css";
 
-const MemberCard = () => {
-  return <></>;
+const MemberCard = (props) => {
+  return (
+    <div className={styles.section}>
+      <h2></h2>
+      <p className={styles.bodytext}>{props.bodytext}</p>
+    </div>
+  );
 };
 
 export default MemberCard;
