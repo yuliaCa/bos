d29d4cc7ad7f8cc0bca4538989c590293b5086c3
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-11-02 13:15:02 -0700

    change member card title

--- client/src/components/Team/MemberCard.js
+++ client/src/components/Team/MemberCard.js
@@ -4,8 +4,17 @@ import styles from "./MemberCard.module.css";
 const MemberCard = (props) => {
   return (
     <div className={styles.section}>
-      <h2></h2>
-      <p className={styles.bodytext}>{props.bodytext}</p>
+      <img src={props.src} alt={props.name} />
+      <div className={styles.content}>
+        <h2>{props.name}</h2>
+        <h3>{props.role}</h3>
+        <p className={styles.bodytext}>{props.bodytext}</p>
+        <ul>
+          <li>{props.link1}</li>
+          <li>{props.link2}</li>
+          <li>{props.link3}</li>
+        </ul>
+      </div>
     </div>
   );
 };
