eb228ff8a40e2bc0f9333b0e278b80010f00eb33
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-11-02 07:47:22 -0700

    Update TeamPage.js

--- client/src/pages/TeamPage.js
+++ client/src/pages/TeamPage.js
@@ -1,5 +1,15 @@
-function TeamPage() {
-    return <div>I am Team Page</div>
-}
+import React from "react";
+import MissionCard from "../components/Team/MissionCard";
+import MemberCard from "../components/Team/MemberCard";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <MissionCard heading="" bodytext="" />
+
+      <MemberCard src="" alt="" heading="" bodytext="" links="" />
+    </div>
+  );
+};
 
 export default TeamPage;
