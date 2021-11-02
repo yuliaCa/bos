7e12cb5fb1d0092017613e9cdc18d31a25918184
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-11-02 09:54:36 -0700

    Update TeamPage.js

--- client/src/pages/TeamPage.js
+++ client/src/pages/TeamPage.js
@@ -5,11 +5,87 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const TeamPage = () => {
+  const [teamContent, setTeamContent] = useState({});
+
+  // [temporary solution] teamContent will be pull from mongodb
+  useEffect(() => {
+    setTeamContent({
+      header: "Our Mission",
+      bodytext:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae laboriosam obcaecati exercitationem harum rerum veniam consectetur excepturi, neque hic, aliquid ipsum voluptate modi nostrum odit quidem provident autem dolorum. Fugit, non aliquid necessitatibus molestiae accusamus velit. Corporis dignissimos voluptates quaerat, assumenda incidunt quis exercitationem iure distinctio vitae accusamus asperiores consequatur.",
+      members: [
+        {
+          name: "Name",
+          role: "Role",
+          intro:
+            "Introduction on themselves lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+          link1: "link1",
+          link2: "link2",
+          link3: "link3",
+        },
+        {
+          name: "Name",
+          role: "Role",
+          intro:
+            "Introduction on themselves lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+          link1: "link1",
+          link2: "link2",
+          link3: "link3",
+        },
+        {
+          name: "Name",
+          role: "Role",
+          intro:
+            "Introduction on themselves lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+          link1: "link1",
+          link2: "link2",
+          link3: "link3",
+        },
+        {
+          name: "Name",
+          role: "Role",
+          intro:
+            "Introduction on themselves lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+          link1: "link1",
+          link2: "link2",
+          link3: "link3",
+        },
+        {
+          name: "Name",
+          role: "Role",
+          intro:
+            "Introduction on themselves lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+          link1: "link1",
+          link2: "link2",
+          link3: "link3",
+        },
+        {
+          name: "Name",
+          role: "Role",
+          intro:
+            "Introduction on themselves lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+          link1: "link1",
+          link2: "link2",
+          link3: "link3",
+        },
+        {
+          name: "Name",
+          role: "Role",
+          intro:
+            "Introduction on themselves lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+          link1: "link1",
+          link2: "link2",
+          link3: "link3",
+        },
+      ],
+    });
+  }, []);
 
   return (
     <div>
-      <MissionCard heading="" bodytext="" />
+      <MissionCard heading="Our Mission" bodytext={teamContent.bodytext} />
 
+      <h2>Our Team</h2>
       <MemberCard src="" alt="" heading="" bodytext="" links="" />
     </div>
   );
