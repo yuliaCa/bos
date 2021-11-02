8739807687b3c3b4f6a4f50b01798933e2d638a6
Doug Leung <dleung46@mylangara.ca>
2021-11-01 22:56:53 -0700

    fix: when user logs in, profile shows up

--- client/src/components/NavBar/Navbar.js
+++ client/src/components/NavBar/Navbar.js
@@ -1,11 +1,21 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 // Link is built-in with react-router-dom, that helps us stay on the same page, without reloading it, and not sent a request to the server every time we click a regular <a> tag. 
 import styles from './Navbar.module.css';
 // styles can be substituted by any other word. It is the variable we declare that becomes an object. Any values from the file will be properties of the object that we can then assign to the components below.
 
 
+import * as firebase from '../../authentication';
+
 function Navbar() {
     // first div is for logo
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    firebase.onAuthStateChanged(firebase.auth, (user) => {
+      return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+    });  
+
     return (
         <header className={styles.header}>
            
@@ -24,10 +34,8 @@ function Navbar() {
                     </li>
                   
                     <li>
-                          
-                        <Link to='/profile'>Profile</Link>
-                
-                    </li>
+                    {!isLoggedIn ? ("") : (<Link to='/profile'>Profile</Link>)}
+                    </li>  
                 </ul>
             </nav>
         </header>
