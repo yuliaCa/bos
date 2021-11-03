bcbd1bfe819b287804846c493abf9f9ff4ea9350
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-11-02 17:06:16 -0700

    Update index.js
    add routes to missionContent

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,9 +1,11 @@
-const router = require('express').Router({ mergeParams: true });
+const router = require("express").Router({ mergeParams: true });
 
 const userProfileRouter = require("./userProfile.js");
 const productRouter = require("./product.js");
 const dailyLogRouter = require("./dailyLog.js");
 const pageContents = require("./pageContent.js");
+
+// const missionContentRouter = require("./missionContent.js")
 
 router.use("/profile", userProfileRouter);
 router.use("/products", productRouter);
@@ -11,5 +13,7 @@ router.use("/register", userProfileRouter);
 router.use("/myroutine/products", productRouter);
 router.use("/dailyroutine", dailyLogRouter);
 router.use("/pagecontents", pageContents);
+
+// router.use("/missioncontent", missionContentRouter);
 
 module.exports = router;
