f8093e2838e3d58ab26bcdc21c7e00d202e0867e
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-11-02 17:05:57 -0700

    Create missionContent.js

---
+++ Backend/routes/missionContent.js
@@ -0,0 +1,16 @@
+// const router = require("express").Router({mergeParams:true});
+// const {
+//   getAllMissionContent,
+//   getMissionContent,
+//   postMissionContent,
+//   deleteMissionContent,
+// } = require("../controllers/missionControllers.js");
+
+// // Team Page: Mission Content 
+
+// router.get("/", getAllMissionContent);
+// router.get("/:id", getMissionContent);
+// router.post("/", postMissionContent);
+// router.delete("/:id", deleteMissionContent)
+
+// module.exports = router;
