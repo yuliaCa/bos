2b9b9b9b33586cc07b1c8cb52b34d268b94ba352
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-11-02 18:37:59 -0700

    Fix bug on dailyLogSchema

--- Backend/models/dailyLogSchema.js
+++ Backend/models/dailyLogSchema.js
@@ -13,7 +13,7 @@ const dailyLogSchema = new dbSchema({
     userEmailAddress: { type: String, required: true },
     dailyLogDate: { type: Date, default: Date.now },
     objRoutineLog: [productSubSchema],
-    overallRate: {type: Integer, default: 0} // integer 1=unhappy, 2=moderate, 3=happy
+    overallRate: {type: Number, default: 0} // integer 1=unhappy, 2=moderate, 3=happy
 });
 
 // .model('name of the model', schemaName, 'Mongoose collection name')
