a60568fd606b898157c10a675990d4cf67077368
Yulia <mail4yulia@yahoo.com>
2021-11-02 20:38:37 -0700

    add user auth as props to myRoutines

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -6,56 +6,56 @@ import { set } from "mongoose";
 import { getAuth } from "firebase/auth";
 
 function ProfilePage() {
-  // in return: let's say 2 buttons. if MyPage clicked - we render myPage section, if myRoutines clicked - we render myRoutines section.
-  // Sounds like useState will be useful here. [defaultSection(myPage), setSection()]
-  const [defaultSection, setSection] = useState("MyPage");
+    // in return: let's say 2 buttons. if MyPage clicked - we render myPage section, if myRoutines clicked - we render myRoutines section.
+    // Sounds like useState will be useful here. [defaultSection(myPage), setSection()]
+    const [defaultSection, setSection] = useState("MyPage");
 
-  const MyPageHandler = () => {
-    setSection("MyPage");
-  };
-  const MyRoutinesHandler = () => {
-    setSection("MyRoutines");
-  };
+    const MyPageHandler = () => {
+        setSection("MyPage");
+    };
+    const MyRoutinesHandler = () => {
+        setSection("MyRoutines");
+    };
 
-  // Get logged user info from firebase auth
-  const auth = getAuth();
-  const user  = auth.currentUser;
-  let displayName;
-  let email;
+    // Get logged user info from firebase auth
+    const auth = getAuth();
+    const user = auth.currentUser;
+    let displayName;
+    let email;
 
     if (user !== null) {
-      // The user object has basic properties such as display name, email, etc.
-      displayName = user.displayName;
-      email = user.email;
-    //   const photoURL = user.photoURL;
-    //   const emailVerified = user.emailVerified;
+        // The user object has basic properties such as display name, email, etc.
+        displayName = user.displayName;
+        email = user.email;
+        //   const photoURL = user.photoURL;
+        //   const emailVerified = user.emailVerified;
 
-      // The user's ID, unique to the Firebase project. Do NOT use
-      // this value to authenticate with your backend server, if
-      // you have one. Use User.getToken() instead.
-      //   const uid = user.uid;
+        // The user's ID, unique to the Firebase project. Do NOT use
+        // this value to authenticate with your backend server, if
+        // you have one. Use User.getToken() instead.
+        //   const uid = user.uid;
     }
 
 
-  return (
-    <div>
-      <h2>Profile</h2>
+    return (
+        <div>
+            <h2>Profile</h2>
 
-      <div>
-        <span className={styles.span} onClick={MyPageHandler}>
-          My Page
+            <div>
+                <span className={styles.span} onClick={MyPageHandler}>
+                    My Page
         </span>
-        <span className={styles.span} onClick={MyRoutinesHandler}>
-          My Routines
+                <span className={styles.span} onClick={MyRoutinesHandler}>
+                    My Routines
         </span>
-      </div>
-      {defaultSection === "MyPage" ? (
-        <MyPage displayName={displayName} email={email} />
-      ) : (
-        <MyRoutines />
-      )}
-    </div>
-  );
+            </div>
+            {defaultSection === "MyPage" ? (
+                <MyPage displayName={displayName} email={email} />
+            ) : (
+                    <MyRoutines displayName={displayName} email={email} />
+                )}
+        </div>
+    );
 }
 
 export default ProfilePage;
