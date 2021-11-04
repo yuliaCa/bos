97ed3819ab478a144b203c560c61d2c9a6ea5ccf
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-11-04 02:21:03 -0700

    Update MyPage.js

--- client/src/pages/profilePage/MyPage.js
+++ client/src/pages/profilePage/MyPage.js
@@ -1,5 +1,5 @@
 import styles from "./MyPage.module.css";
-import { FaMapMarkerAlt } from "react-icons/fa";
+import { MdOutlineLocationOn } from "react-icons/md";
 import Weather from "../../components/Weather/Weather.js";
 import WeatherChart from "../../components/Charts/WeatherChart";
 import UsageChart from "../../components/Charts/UsageChart";
@@ -10,15 +10,14 @@ function MyPage(props) {
 
   return (
     <div className={styles.myPageSection}>
-      <h1 className={styles.heading}>My Page</h1>
       <div className={styles.userSection}>
         <div className={styles.profileImage} />
         <div className={styles.profileData}>
           <ul>
-            <li>{`${props.displayName} ${props.email}`}</li>
-            <li>
-              <FaMapMarkerAlt />
-              {city}
+            <li className={styles.profileName}>{`${props.displayName}`}</li>
+            <li className={styles.city}>
+              <MdOutlineLocationOn className={styles.locIcon} />
+              <div>{city}</div>
             </li>
           </ul>
           <ul className={styles.skinTypes}>
@@ -29,7 +28,13 @@ function MyPage(props) {
         </div>
       </div>
       <Weather />
+      <div className={styles.headerWrap}>
+        <h2>Weather</h2>
+      </div>
       <WeatherChart />
+      <div className={styles.headerWrap}>
+        <h2>Product Usage</h2>
+      </div>
       <UsageChart />
     </div>
   );
