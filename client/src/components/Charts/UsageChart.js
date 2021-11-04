f4f9ddc30d8edeef1de567543c7d8a408dba6cde
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-11-04 02:22:28 -0700

    Update UsageChart.js

--- client/src/components/Charts/UsageChart.js
+++ client/src/components/Charts/UsageChart.js
@@ -5,20 +5,20 @@ import { Bar } from "react-chartjs-3";
 const UsageChart = () => {
   return (
     <div className={styles.chartStyle}>
-      <Bar
+      <Bar className={styles.chart}
         data={{
           labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
           datasets: [
             {
               label: "Cleanser",
-              data: [1, 2, 1, 2, 1, 0, 1],
+              data: [1, 1, 1, 1, 1, 0, 1],
               backgroundColor: "rgba(243, 186, 124, 0.5)",
               borderColor: "rgba(243, 186, 124, 0.5)",
               borderWidth: 0,
             },
             {
               label: "Serum",
-              data: [1, 1, 1, 0, 1, 0, 1],
+              data: [1, 0, 1, 0, 0, 0, 0],
               backgroundColor: "rgba(209, 152, 136, 0.5)",
               borderColor: "rgba(209, 152, 136, 0.5)",
               borderWidth: 0,
@@ -32,23 +32,23 @@ const UsageChart = () => {
             },
             {
               label: "Cream",
-              data: [1, 1, 1, 0, 1, 1, 0],
+              data: [0, 1, 1, 0, 1, 1, 0],
               backgroundColor: "rgba(247, 237, 225, 1)",
               borderColor: "rgba(247, 237, 225, 1)",
               borderWidth: 0,
             },
           ],
         }}
-        height={300}
-        width={600}
+        height={150}
+        width={400}
         options={{
           maintainAspectRatio: true,
           layout: {
             padding: {
-              top: 50,
-              left: 200,
-              right: 200,
-              bottom: 50,
+              top: 0,
+              left: 100,
+              right: 100,
+              bottom: 0,
             },
           },
           scales: {
