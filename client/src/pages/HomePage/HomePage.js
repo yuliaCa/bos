41daec9cc3552850ac85b5caf63e29a167037515
Doug Leung <dleung46@mylangara.ca>
2021-11-06 15:41:05 -0700

    Feat: pagecontent linked to mongo

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HeroSection from "./HeroSection/HeroSection";
 import FeaturesSection from "./FeaturesSection/FeaturesSection";
 import InsightsSection from "./InsightsSection/InsightsSection";
 import StartRoutineSection from "../../components/StartRoutineSection/StartRoutineSection";
-
+import axios from "axios";
 
 function HomePage() {
+     
+    const [insightsSectionContent, setinsightsSectionContent] = useState({});
+ 
+
+
+    useEffect(() => {
+
+    axios
+      .get("/pagecontents/")
+      .then((result) => {
+      setinsightsSectionContent(result.data[3]);
+      })
+      .catch((error) => console.log(error));
+
+    }, []);
+
     return <div>
 
         <HeroSection src="https://s3-us-west-2.amazonaws.com/bos-skincare/home/hero-image/1920.1080.jpg" alt="hero image" />
 
         <FeaturesSection />
 
-        <InsightsSection src="https://s3-us-west-2.amazonaws.com/bos-skincare/homepage/insight_home_bigsize.png" alt="insights image" heading="We talk about insights here" bodytext="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae laboriosam obcaecati exercitationem harum rerum veniam consectetur excepturi, neque hic, aliquid ipsum voluptate modi nostrum odit quidem provident autem dolorum. Fugit, non aliquid necessitatibus molestiae accusamus velit. Corporis dignissimos voluptates quaerat, assumenda incidunt quis exercitationem iure distinctio vitae accusamus asperiores consequatur."/>
+        <InsightsSection src="https://s3-us-west-2.amazonaws.com/bos-skincare/homepage/insight_home_bigsize.png" alt="insights image" heading={insightsSectionContent.header} bodytext={insightsSectionContent.body}/>
        
         <StartRoutineSection />
 
