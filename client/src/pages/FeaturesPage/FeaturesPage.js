c0c7d2190d066939db4e518b3c29f7b85ffaf97c
Doug Leung <dleung46@mylangara.ca>
2021-11-06 15:41:05 -0700

    Feat: pagecontent linked to mongo

--- client/src/pages/FeaturesPage/FeaturesPage.js
+++ client/src/pages/FeaturesPage/FeaturesPage.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FeatureInfoSection from "./FeaturesInfoSections/FeatureInfo";
 import FeatureInfo2Section from "./FeaturesInfoSections/FeatureInfo2";
-import StartRoutineSection from "../../components/StartRoutineSection/StartRoutineSection";
+import axios from "axios";
 
 function FeaturesPage() {
+
+    const [routineContent, setRoutineContent] = useState({});
+    const [adviceContent, setAdviceContent] = useState({});
+    const [insightsContent, setInsightsContent] = useState({});
+
+
+    useEffect(() => {
+
+    axios
+      .get("/pagecontents/")
+      .then((result) => {
+         
+      setRoutineContent(result.data[4]);
+      setAdviceContent(result.data[5]);
+      setInsightsContent(result.data[6]);
+      })
+      .catch((error) => console.log(error));
+
+    }, []);
+
+
     return <div>
         
-        <FeatureInfo2Section src="https://s3-us-west-2.amazonaws.com/bos-skincare/feature/big-images/routine.png" alt="Feature One image" heading="Product Routines" bodytext="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae laboriosam obcaecati exercitationem harum rerum veniam consectetur excepturi, neque hic, aliquid ipsum voluptate modi nostrum odit quidem provident autem dolorum. Fugit, non aliquid necessitatibus molestiae accusamus velit. Corporis dignissimos voluptates quaerat, assumenda incidunt quis exercitationem iure distinctio vitae accusamus asperiores consequatur." buttonTitle="START YOUR ROUTINE"/>
+        <FeatureInfo2Section src="https://s3-us-west-2.amazonaws.com/bos-skincare/feature/big-images/routine.png" alt="Feature One image" heading={routineContent.header} bodytext={routineContent.body} buttonTitle="START YOUR ROUTINE"/>
         
-        <FeatureInfoSection src="https://s3-us-west-2.amazonaws.com/bos-skincare/feature/big-images/weather_advice.png" alt="Feature Two image" heading="Weather Advice" bodytext="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae laboriosam obcaecati exercitationem harum rerum veniam consectetur excepturi, neque hic, aliquid ipsum voluptate modi nostrum odit quidem provident autem dolorum. Fugit, non aliquid necessitatibus molestiae accusamus velit. Corporis dignissimos voluptates quaerat, assumenda incidunt quis exercitationem iure distinctio vitae accusamus asperiores consequatur." buttonTitle="CREATE YOUR PROFILE"/>
+        <FeatureInfoSection src="https://s3-us-west-2.amazonaws.com/bos-skincare/feature/big-images/weather_advice.png" alt="Feature Two image" heading={adviceContent.header} bodytext={adviceContent.body} buttonTitle="CREATE YOUR PROFILE"/>
         
-        <FeatureInfo2Section src="https://s3-us-west-2.amazonaws.com/bos-skincare/feature/big-images/insight.png" alt="Feature Three image" heading="Insights" bodytext="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae laboriosam obcaecati exercitationem harum rerum veniam consectetur excepturi, neque hic, aliquid ipsum voluptate modi nostrum odit quidem provident autem dolorum. Fugit, non aliquid necessitatibus molestiae accusamus velit. Corporis dignissimos voluptates quaerat, assumenda incidunt quis exercitationem iure distinctio vitae accusamus asperiores consequatur." buttonTitle="START YOUR FIRST WEEK"/>
+        <FeatureInfo2Section src="https://s3-us-west-2.amazonaws.com/bos-skincare/feature/big-images/insight.png" alt="Feature Three image" heading={insightsContent.header} bodytext={insightsContent.body} buttonTitle="START YOUR FIRST WEEK"/>
         
         </div>
 }
--- client/src/pages/HomePage/FeaturesSection/FeaturesSection.js
+++ client/src/pages/HomePage/FeaturesSection/FeaturesSection.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Features.module.css';
 import Feature from "./Feature/Feature";
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 function FeaturesSection() {
   
+    const [routineContent, setRoutineContent] = useState({});
+    const [adviceContent, setAdviceContent] = useState({});
+    const [insightsContent, setInsightsContent] = useState({});
+
+
+    useEffect(() => {
+
+    axios
+      .get("/pagecontents/")
+      .then((result) => {
+         
+      setRoutineContent(result.data[0]);
+      setAdviceContent(result.data[1]);
+      setInsightsContent(result.data[2]);
+      })
+      .catch((error) => console.log(error));
+
+    }, []);
+
     return (
   
     <div className={styles.featureSection}>
@@ -12,9 +32,9 @@ function FeaturesSection() {
         <h2 className={styles.featureHeading}>FEATURES</h2>
 
         <div className={styles.featureCards}>
-        <Feature className={styles.feat1} src="https://s3-us-west-2.amazonaws.com/bos-skincare/home/routine_icon.svg" alt="routine feature image" heading="Product Routines" bodytext="Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit." />
-        <Feature className={styles.feat2} src="https://s3-us-west-2.amazonaws.com/bos-skincare/home/weatheradvice_icon.svg" alt="advice feature image" heading="Weather Advice" bodytext="Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit." />
-        <Feature className={styles.feat3} src="https://s3-us-west-2.amazonaws.com/bos-skincare/home/insight_icon.svg" alt="insights feature image" heading="Insights" bodytext="Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit." />
+        <Feature className={styles.feat1} src="https://s3-us-west-2.amazonaws.com/bos-skincare/home/routine_icon.svg" alt="routine feature image" heading={routineContent.header} bodytext={routineContent.body} />
+        <Feature className={styles.feat2} src="https://s3-us-west-2.amazonaws.com/bos-skincare/home/weatheradvice_icon.svg" alt="advice feature image" heading={adviceContent.header} bodytext={adviceContent.body} />
+        <Feature className={styles.feat3} src="https://s3-us-west-2.amazonaws.com/bos-skincare/home/insight_icon.svg" alt="insights feature image" heading={insightsContent.header} bodytext={insightsContent.body} />
         </div>
 
         <button className={styles.register}>
