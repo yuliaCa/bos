8832566f4b661787dffe9d1034fd6f487f393602
Yulia <mail4yulia@yahoo.com>
2021-11-08 10:38:25 -0800

    #48 added min-height to product details div

--- client/src/components/MyRoutine/ProductDetails.js
+++ client/src/components/MyRoutine/ProductDetails.js
@@ -1,11 +1,11 @@
-
+import styles from './ProductDetails.module.css';
 
 const ProductDetails = (props) => {
 
 
 
     return (
-        <div>
+        <div className={styles.productDetails}>
             <a href='#' onClick={props.evening ? props.closeDetailsEvening : props.closeDetailsMorning}>Back To Routine</a>
 
 
