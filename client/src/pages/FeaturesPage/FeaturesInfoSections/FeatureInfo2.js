2fcd9a9031fe2e5cc55618962f512208d4a8bb1a
Doug Leung <dleung46@mylangara.ca>
2021-11-13 20:28:40 -0800

    fix: link changes when logged in

--- client/src/pages/FeaturesPage/FeaturesInfoSections/FeatureInfo2.js
+++ client/src/pages/FeaturesPage/FeaturesInfoSections/FeatureInfo2.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FeatureInfo2.module.css';
 import { Link } from 'react-router-dom';
 
+//import functions from Firebase authentication SDK
+import * as firebase from '../../../authentication.js';
+
 function FeatureInfoSection2(props) {
-  
+ 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    
+    firebase.onAuthStateChanged(firebase.auth, (user) => {
+        return user ? setIsLoggedIn(true) : setIsLoggedIn(false)
+    });
+    
     return (
         <div className={styles.featureOuter}>
             <div className={styles.content}>
                 <h2>{props.heading}</h2>
                 <p>{props.bodytext}</p>
                 <button>
-                    <Link to='/login'>{props.buttonTitle}</Link>
-                </button>
+                    {isLoggedIn ? (
+                    <Link to={props.link}>{props.buttonTitle}</Link>
+                    ) : (
+                    <Link to='/registration'>{props.buttonTitle}</Link>
+                    )}
+                </button> 
             </div>
 
             <div className={styles.imageContainer}>
