81b3dd4e2534eca8eea7f9a4d0820cdbbe6e5586
Javier Gongora <jgongora@gmail.com>
2021-11-14 20:03:10 -0800

    undo settings

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -21,6 +21,4 @@ router.use("/missioncontent", missionContentRouter);
 router.use("/teamcontent", teamContentRouter);
 router.use("/advicecontent", adviceContentRouter);
 
-router.use("/settings", settingsRouter);
-
 module.exports = router;
