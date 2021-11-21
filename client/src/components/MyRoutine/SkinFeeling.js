be19ec571b116e811ca89172020ca46f1dc63209
Javier Gongora <jgongora@gmail.com>
2021-11-20 19:11:33 -0800

    Add skin feeling selector

---
+++ client/src/components/MyRoutine/SkinFeeling.js
@@ -0,0 +1,44 @@
+import styles from "./SkinFeeling.module.css";
+import React, { useState } from "react";
+
+const SkinFeeling = (props) => {
+  const bosStorage = "https://s3-us-west-2.amazonaws.com/bos-skincare";
+
+  const [sadFeeling, setSadFeeling] = useState(false);
+  const [neutralFeeling, setNeutralFeeling] = useState(false);
+  const [happyFeeling, setHappyFeeling] = useState(false);
+
+  const onClickFeeling = (e) => {
+    console.log(e.alt);
+  };
+
+  return (
+    <div className={styles.skinFeelingSection}>
+    <div>
+      <h2 className={styles.skinFeelingH2}>How was the skin of the day?</h2>
+    </div>
+      <div className={styles.smileyStyle}>
+        <ul className={styles.skinFeelingUl}>
+          <li key="sad">
+            <img src={`${bosStorage}/icons/sad.svg`} alt="smiley" />
+          </li>
+          <li key="neutral">
+            <img src={`${bosStorage}/icons/neutral.svg`} alt="smiley" />
+          </li>
+          <li key="happy">
+            <img
+              src={`${bosStorage}/icons/happy.svg`}
+              alt="smiley"
+              onClick={onClickFeeling}
+            />
+          </li>
+        </ul>
+        <div>
+          <button className={styles.skinFeelingButton}>Save</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SkinFeeling;
