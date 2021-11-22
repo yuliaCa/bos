c9ba2ad8deea09ebbac54871792ccf03e3c84d25
Javier Gongora <jgongora@gmail.com>
2021-11-22 10:13:03 -0800

    skinFeeling selector stick bg colour and styling

--- client/src/components/MyRoutine/SkinFeeling.js
+++ client/src/components/MyRoutine/SkinFeeling.js
@@ -4,56 +4,48 @@ import React, { useState } from "react";
 const SkinFeeling = (props) => {
   const bosStorage = "https://s3-us-west-2.amazonaws.com/bos-skincare";
 
-  const [sadFeeling, setSadFeeling] = useState(false);
-  const [neutralFeeling, setNeutralFeeling] = useState(false);
-  const [happyFeeling, setHappyFeeling] = useState(false);
+  const [currentFeeling, setCurrentFeeling] = useState(" ");
   const [skinFeelingSaved, setSkinFeelingSaved] = useState(false);
 
+
   const onClickFeeling = (e) => {
-    console.log(e.alt);
+    e.preventDefault();
+    setCurrentFeeling(e.target.value);
+
   };
 
-  const onClickFeelingSave = () => {
+  const onClickFeelingSave = (e) => {
+    e.preventDefault();
     setSkinFeelingSaved(!skinFeelingSaved);
+
   };
 
   return (
     <div className={styles.skinFeelingSection}>
       <div>
-        <h2 className={styles.skinFeelingH2}>How was the skin of the day?</h2>
+        <h2 className={styles.skinFeelingH2}>How is your skin today?</h2>
       </div>
       <div className={styles.smileyStyle}>
-        <form submit={onClickFeelingSave} onChange={()=>onClickFeeling()}>
-          <label>
+        <form onChange={e=>onClickFeeling(e)}>
+          <label className={currentFeeling==="Not so good"&&styles.skinFeelingStickBg}>
             <input type="radio" name="skinFeeling" value="Not so good" />
-            <img
-              src={`${bosStorage}/icons/sad.svg`}
-              alt="Sad Smiley"
-            />
+            <img src={`${bosStorage}/icons/sad.svg`} alt="Sad Smiley" />
           </label>
-          <label>
+          <label className={currentFeeling==="Feeling OK"&&styles.skinFeelingStickBg}>
             <input type="radio" name="skinFeeling" value="Feeling OK" />
-            <img
-              src={`${bosStorage}/icons/neutral.svg`}
-              alt="Neutral Smiley"
-            />
+            <img src={`${bosStorage}/icons/neutral.svg`} alt="Neutral Smiley" />
           </label>
-          <label>
+          <label className={currentFeeling==="Feeling good"&&styles.skinFeelingStickBg}>
             <input type="radio" name="skinFeeling" value="Feeling good" />
-            <img
-              src={`${bosStorage}/icons/happy.svg`}
-              alt="Happy Smiley"
-            />
+            <img src={`${bosStorage}/icons/happy.svg`} alt="Happy Smiley"  />
           </label>
+          <h3 className={styles.currentFeeling}>{`${currentFeeling}`}</h3>
+          <div className={styles.skinFeelingButtonWrap}>
+            <button className={styles.skinFeelingButton} onClick={onClickFeelingSave}>
+              {skinFeelingSaved ? "SAVED" : "SAVE"}
+            </button>
+          </div>
         </form>
-        <div className={styles.skinFeelingButtonWrap}>
-          <button
-            className={styles.skinFeelingButton}
-            
-          >
-            {skinFeelingSaved ? "SAVED" : "SAVE"}
-          </button>
-        </div>
       </div>
     </div>
   );
