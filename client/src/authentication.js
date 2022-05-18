8d63ff1598b8afbbea373eecc16bd338d85061a6
Javier Gongora <jgongora@gmail.com>
2022-05-17 21:20:15 -0700

    add access to environment variables

--- client/src/authentication.js
+++ client/src/authentication.js
@@ -11,12 +11,12 @@ console.log("Connected to firebase.js");
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDsE80-v0O03bHT9hV5SpviW0-K_XWQFdc",
-    authDomain: "bureau-of-skincare.firebaseapp.com",
-    projectId: "bureau-of-skincare",
-    storageBucket: "bureau-of-skincare.appspot.com",
-    messagingSenderId: "722992197009",
-    appId: "1:722992197009:web:4d7e4571d3df59c16f7e30"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
