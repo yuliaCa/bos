992c08d1f36df6b8d6e6a1c66f80d5ca39374b70
Javier Gongora <jgongora@gmail.com>
2022-05-20 15:24:57 -0700

    update files

--- Server/Backend/connection.js
+++ Server/Backend/connection.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 
-let mongoDB = `mongodb+srv://msuria00:e57hEbZDrhhY3Wfj@cluster0.6v3kl.mongodb.net/bosDB?retryWrites=true&w=majority`;
+let mongoDB = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_ID_HOST}.mongodb.net/bosDB?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("Connected to Atlas!");
